312ef8ec9d47a0ebf20ecf9d7c3096515d07dd61
JulianOspino <julianospino5@gmail.com>
2022-06-09 23:51:55 +0000

    Pago nequi (falta qr)

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -70,15 +70,9 @@
                             </div>
                         </div>
                         <div id="paypal" class="tab-pane fade pt-3">
-                            <h6 class="pb-2">Select your paypal account type</h6>
-                            <div class="form-group "> <label class="radio-inline"> <input type="radio"
-                                        name="optradio" checked> Domestic </label> <label class="radio-inline"> <input
-                                        type="radio" name="optradio" class="ml-5">International </label></div>
-                            <p> <button type="button" class="btn btn-primary "><i class="fab fa-paypal mr-2"></i> Log
-                                    into my Paypal</button> </p>
-                            <p class="text-muted"> Note: After clicking on the button, you will be directed to a
-                                secure gateway for payment. After completing the payment process, you will be redirected
-                                back to the website to view details of your order. </p>
+                            <h6 class="pb-2">Escanea el siguiente qr en tu aplicación</h6>
+                            <p> <button type="submit" class="btn btn-primary ">Completar pago</button> </p>
+                            <p class="text-muted"> Nota: </p>
                         </div>
                     </div>
                 </div>
